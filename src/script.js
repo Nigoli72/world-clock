1f5b767997bc2eda5f8c97ea8efccdee8278d1c2
Nigoli72 <125713703+Nigoli72@users.noreply.github.com>
2023-03-14 20:48:40 -0400

    styling updates

--- src/script.js
+++ src/script.js
@@ -1,21 +1,19 @@
 function updateTime() {
   //Tokyo
   let tokyoTime = moment.tz("Asia/Tokyo");
-
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>] A[</small>]");
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do[,] YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd MMMM D[,] YYYY");
+
   //Sydney
   let sydneyTime = moment.tz("Australia/Sydney");
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss[<small>] A[</small>]"
-  );
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do[,] YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss A");
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd MMMM D[,] YYYY");
 }
 
 updateTime();
